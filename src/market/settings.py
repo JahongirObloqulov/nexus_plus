6694e3cd119782c0c6e00d377c32241f294731f2
JahongirObloqulov <jahongir.obloqulov13@gmail.com>
2024-02-19 13:54:38 +0500

    product app added

--- src/market/settings.py
+++ src/market/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
-    'user'
+    'user',
+    'product'
 ]
 
 MIDDLEWARE = [
--- src/user/models.py
+++ src/user/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 # Create your models here.
@@ -14,20 +14,22 @@ class UserManager(BaseUserManager):
         user.save()
         return user
 
-    def create_superuser(self, username, email, password):
+    def create_superuser(self, username, password, email="admin@admin.uz" ):
         user = self.create_user(username, email, password)
         user.is_superuser = True
         user.save()
         return user
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     firstname = models.CharField(max_length=30, null=True, blank=True)
     lastname = models.CharField(max_length=30, null=True, blank=True)
     username = models.CharField(max_length=30, unique=True, null=False)
     photo = models.ImageField(upload_to="images/", null=True)
     email = models.EmailField(null=False, blank=False)
     phone_number = models.CharField(max_length=13, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
     joined_date = models.DateTimeField(auto_now_add=True)
 
     objects = UserManager()
@@ -35,4 +37,8 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.username
+
+    @property
+    def is_staff(self):
+        return self.is_superuser
 
